3d59161ef1019e35c8caff06f799105f4019a7df
Alexander-Ezeh <alexanderikennaefunaezeh@gmail.com>
2023-03-11 22:38:11 -0800

    libmy

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,3 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-/*
- * File: main.h
- * Auth: Alexander-Ezeh
- * Desc: Header file containing the prototypes of all functions included in the static library main.h
- *
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,5 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif 
