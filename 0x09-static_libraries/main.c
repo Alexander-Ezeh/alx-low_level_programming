6d7c618d0662064725f1826102c093ae38014896
Alexander-Ezeh <alexanderikennaefunaezeh@gmail.com>
2023-03-13 23:50:23 -0700

    tasl 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int main(void)
+/*
+ * File: main.h
+ * Auth: Alex
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
+
+int main(_puts)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
