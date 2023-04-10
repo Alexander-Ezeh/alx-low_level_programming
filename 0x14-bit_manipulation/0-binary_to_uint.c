2147be5e2a3664a868c6b92792d8e639c38c7e64
Alexander-Ezeh <alexanderikennaefunaezeh@gmail.com>
2023-04-10 13:14:14 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	int len;
+	unsigned int num;
 
-	if (b[len] == '\0')
+	num = 0;
+	if (!b)
 		return (0);
-
-	while ((b[len] == '0') || (b[len] == '1'))
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+	for (len = 0; b[len] != '\0'; len++)
 	{
 		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		if (b[len] == '1')
+			num += 1;
 	}
-
 	return (num);
 }
